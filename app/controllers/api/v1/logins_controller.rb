2ab25134cc89ab1968951b4b22ba1e5cbf3cddee
jslack2537 <jslack2537@gmail.com>
2020-04-01 11:00:50 +0000

    user finished but login still needs work

--- app/controllers/api/v1/logins_controller.rb
+++ app/controllers/api/v1/logins_controller.rb
@@ -2,9 +2,15 @@
 
 class Api::V1::LoginsController < ApplicationController
   def index
-    @logins = Login.count
+    @logins = Login.where(login_params)
     respond_to do |format|
       format.json { render json: @logins.to_json, status: :ok }
     end
+  end
+
+  private
+
+  def login_params
+    params.permit(:user_id, :date)
   end
 end
--- app/models/login.rb
+++ app/models/login.rb
@@ -2,4 +2,5 @@
 
 class Login < ApplicationRecord
   belongs_to :user
+  scope :day_of_week, ->(day_of_week) { where("strftime('%w', date) = ?", day_of_week.to_s) }
 end
